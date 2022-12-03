dcbbec90a7f5cda12afdbad43b0986a007d86a18
Chris Gresty <chris@gresty.dev>
2022-12-03 20:01:09 +0000

    Day 3 chunked!

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -1,6 +1,7 @@
 package dev.gresty.aoc.adventofcode2022.day03
 
 import dev.gresty.aoc.adventofcode2022.execute
+import java.lang.IllegalStateException
 
 fun main() {
     execute(::solve03A, "day03.txt")
@@ -9,28 +10,34 @@ fun main() {
 
 fun solve03A(input: Sequence<String>): Int {
     return input.map { divide(it) }
-        .map { findCommonItem(it) }
+        .map { findCommonItem(it[0], it[1]) }
         .map { priority(it) }
         .sum()
 }
 
 fun solve03B(input: Sequence<String>): Int {
-    return input.runningFold(listOf<String>()) { acc, s -> if (acc.size < 3) acc + s else listOf(s) }
-        .filter { it.size == 3 }
-        .map { Triple(it[0], it[1], it[2]) }
-        .map { findCommonItem(it) }
+    return input.chunked(3)
+        .map { findCommonItem(it[0], it[1], it[2]) }
         .map { priority(it) }
         .sum()
 }
 
 fun divide(source: String) =
-    source.substring(0, source.length / 2) to source.substring(source.length / 2)
+    listOf(source.substring(0, source.length / 2), source.substring(source.length / 2))
 
-fun findCommonItem(items: Pair<String, String>) =
-    items.first.asSequence().filter { items.second.contains(it) }.first()
+fun findCommonItem(items1: String, items2: String) : Char {
+    items1.forEach {
+        if (items2.contains(it)) return it
+    }
+    throw IllegalStateException("No common items!")
+}
 
-fun findCommonItem(items: Triple<String, String, String>) =
-    items.first.asSequence().filter { items.second.contains(it) && items.third.contains(it) }.first()
+fun findCommonItem(items1: String, items2: String, items3: String) : Char {
+    items1.forEach {
+        if (items2.contains(it) && items3.contains(it)) return it
+    }
+    throw IllegalStateException("No common items!")
+}
 
 fun priority(item: Char) =
     if (item >= 'a') item - 'a' + 1 else item - 'A' + 27
