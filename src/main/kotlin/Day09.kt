19b69ce4ceadac9f55ffb1c17a1f86f5663b2c1a
Chris Gresty <chris.gresty@anaplan.com>
2022-12-09 15:49:07 +0000

    Day 9 a bit quicker

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -24,34 +24,31 @@ class Day09 : Day<Int> {
         private val adjacent = -1 .. 1
 
         fun moveHead(instr: String) : Rope {
-            val direction = instr[0]
-            val steps = instr.substring(2).toInt()
-            for (i in 1 ..steps) {
-                moveHeadOneSpace(direction)
-                for (j in 1 .. tailEnd) { moveTail(j) }
-                visited.add(x[tailEnd] to y[tailEnd])
-            }
+            moveHead(instr[0], instr.substring(2).toInt())
             return this
         }
 
         fun visited() = visited.size
 
-        private fun moveHeadOneSpace(direction: Char) {
+        private fun moveHead(direction: Char, steps: Int) {
             when (direction) {
-                'U' -> y[0]++
-                'D' -> y[0]--
-                'L' -> x[0]--
-                'R' -> x[0]++
+                'U' -> for (i in 1..steps) { y[0]++ ; moveTail() }
+                'D' -> for (i in 1..steps) { y[0]-- ; moveTail() }
+                'L' -> for (i in 1..steps) { x[0]-- ; moveTail() }
+                'R' -> for (i in 1..steps) { x[0]++ ; moveTail() }
                 else -> throw RuntimeException("Unexpected direction $direction")
             }
         }
 
-        private fun moveTail(i: Int) {
-            val dx = x[i - 1] - x[i]
-            val dy = y[i - 1] - y[i]
-            if (dx in adjacent && dy in adjacent) return
-            x[i] += dx.sign
-            y[i] += dy.sign
+        private fun moveTail() {
+            for (i in 1 .. tailEnd) {
+                val dx = x[i - 1] - x[i]
+                val dy = y[i - 1] - y[i]
+                if (dx in adjacent && dy in adjacent) return
+                x[i] += dx.sign
+                y[i] += dy.sign
+            }
+            visited.add(x[tailEnd] to y[tailEnd])
         }
     }
 }
