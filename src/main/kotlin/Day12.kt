d384a609ab3920e321b38f1dfa6077cc8670befb
Chris Gresty <chris.gresty@anaplan.com>
2022-12-12 10:46:20 +0000

    Day 12 setup

---
+++ src/main/kotlin/Day12.kt
@@ -0,0 +1,16 @@
+package dev.gresty.aoc.adventofcode2022
+
+fun main() {
+    execute(12) { Day12().solveA(it) }
+    execute(12) { Day12().solveB(it) }
+}
+
+class Day12 : Day<Int, Int> {
+    override fun solveA(input: Sequence<String>): Int {
+        return 0
+    }
+
+    override fun solveB(input: Sequence<String>): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/Day12Tests.kt
@@ -0,0 +1,25 @@
+package dev.gresty.aoc.adventofcode2022
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class Day12Tests : FunSpec () {
+
+    private val example1 = """
+        Sabqponm
+        abcryxxl
+        accszExk
+        acctuvwj
+        abdefghi
+    """.trimIndent()
+
+    init {
+        test("Execute example A") {
+            Day12().solveA(example1.lineSequence()).shouldBe(31)
+        }
+
+        test("Execute example B") {
+            Day12().solveB(example1.lineSequence()).shouldBe(0)
+        }
+    }
+}
