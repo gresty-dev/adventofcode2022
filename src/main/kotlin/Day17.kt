64da86701745d5e656e6918a47e1d101a14964bf
Chris Gresty <chris@gresty.dev>
2022-12-19 19:28:07 +0000

    Day 17 tidy up

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -11,43 +11,45 @@ class Day17 : Day<Long, Long> {
     }
 
     override fun solveB(input: Sequence<String>): Long {
-        return solve(input, 1000000000000L)
+        return solve(input, 1_000_000_000_000L)
     }
 
-    fun solve(input: Sequence<String>, blocks: Long): Long {
+    private fun solve(input: Sequence<String>, blocks: Long): Long {
         val rocks = RockFactory()
         val jet = Jet(input.first())
-        val cave = Cave(7, jet)
-        val states = mutableMapOf(CycleState(0, 0, cave.profile(8)) to (0 to 0))
-        var repeatStart: Pair<Int, Int>? = null
-        var repeatEnd: Pair<Int, Int>? = null
+        val cave = Cave(jet)
+        val states = mutableMapOf<CaveState, CycleState>()
+
+        val precycle = CycleState(-1, -1)
+        var cycleStart = precycle
+        var cycleEnd = precycle
         var index = 0
-        do {
-            index++
+        while (cycleEnd == precycle) {
             cave.add(rocks.next())
-            val newState = CycleState(rocks.nextIndex(), jet.nextIndex(), cave.profile(8))
-            states[newState]?.let {
-                repeatStart = it
-                repeatEnd = index to cave.top
+            val caveState = CaveState(rocks.nextIndex(), jet.nextIndex(), cave.profile())
+            val cycleState = CycleState(++index, cave.top)
+            states[caveState]?.let {
+                cycleStart = it
+                cycleEnd = cycleState
             }
-            states[newState] = index to cave.top
-        } while (repeatEnd == null)
+            states[caveState] = cycleState
+        }
 
-        val cycleStart = repeatStart!!.first
-        val cycleLength = repeatEnd!!.first - repeatStart!!.first
+        val cycleLength = cycleEnd.index - cycleStart.index
+        val (cycles, extras) = divmod(blocks - cycleStart.index, cycleLength)
 
-        val cycles = (blocks - cycleStart) / cycleLength
-        val extras = ((blocks - cycleStart) % cycleLength).toInt()
-
-        val preludeHeight = repeatStart!!.second
-        val cycleHeight = repeatEnd!!.second - repeatStart!!.second
+        val cycleHeight = cycleEnd.height - cycleStart.height
         repeat(extras) { cave.add(rocks.next()) }
-        val extrasHeight = cave.top - repeatEnd!!.second
+        val extrasHeight = cave.top - cycleEnd.height
 
-        return preludeHeight + cycles * cycleHeight + extrasHeight
+        return cycleStart.height + cycles * cycleHeight + extrasHeight
     }
 
-    data class CycleState(val rock: Int, val jet: Int, val profile: List<Int>)
+    private fun divmod(dividend: Long, divisor: Int) = Pair(dividend / divisor, (dividend % divisor).toInt())
+
+    data class CaveState(val rock: Int, val jet: Int, val profile: List<Int>)
+    data class CycleState(val index: Int, val height: Int)
+    data class Rock(val shape: List<Int>)
 
     /**
      * 0: left wall
@@ -55,74 +57,79 @@ class Day17 : Day<Long, Long> {
      * 8: right wall
      * row 0: floor
      */
-    class Cave(width: Int, val jet: Jet) {
-        val topGap = 3 + 4
-        val rightWall = 1 shl (width + 1)
-        val cave = mutableListOf(rightWall - 1, 0, 0, 0, 0, 0, 0, 0)
+    class Cave(private val jet: Jet) {
+        private val profileRows = 8
+        private val width = 7
+        private val topGap = 3 + 4
+        private val rightWall = 1 shl (width + 1)
+        private val cave = mutableListOf(rightWall - 1)
+        private var rock = mutableListOf<Int>()
 
-        var rock = mutableListOf<Int>()
         var top = 0
         var x = 0
         var y = 0
 
-        fun add(newRock: Rock) : Int {
+        init { extendCaveSpace() }
+
+        fun add(newRock: Rock) {
             x = 0
             y = top + 4
             rock.clear()
             rock.addAll(newRock.shape)
-            moveRight()
-            moveRight()
-            moveRight()
+            repeat(3) { moveRight() }
             do {
                 if (jet.next() == '<') moveLeft() else moveRight()
             } while (moveDown())
-            rock.indices.forEach { i -> cave[y + i] = cave[y + i] or rock[i] }
-            top = maxOf(top, y + rock.size - 1)
-            while (cave.size <= top + topGap) cave.add(0)
-            return top
+            embedRock()
+            extendCaveSpace()
         }
 
-        fun profile(rows: Int) = (top downTo top - 8).map { if (it > 0) cave[it] else 0 }.toList()
+        fun profile() = (top downTo top - profileRows).map { if (it > 0) cave[it] else 0 }.toList()
 
-        fun moveLeft() =
+        private fun extendCaveSpace() {
+            while (cave.size <= top + topGap) cave.add(0)
+        }
+
+        private fun embedRock() {
+            rock.indices.forEach { i -> cave[y + i] = cave[y + i] or rock[i] }
+            top = maxOf(top, y + rock.size - 1)
+        }
+
+        private fun moveLeft() {
             if (rock.withIndex().all { it.value % 4 == 0 && (it.value / 2 and cave[y + it.index] == 0) }) {
                 rock.indices.forEach { i -> rock[i] /= 2 }
                 x--
-                true
-            } else false
+            }
+        }
 
-
-        fun moveRight() =
+        private fun moveRight() {
             if (rock.withIndex().all { it.value < (rightWall / 2) && (2 * it.value and cave[y + it.index] == 0) }) {
                 rock.indices.forEach { i -> rock[i] *= 2 }
                 x++
-                true
-            } else false
+            }
+        }
 
-
-        fun moveDown() =
+        private fun moveDown() =
             if (rock.indices.all { i -> (rock[i] and cave[y + i - 1]) == 0 }) {
                 y--
                 true
             } else false
     }
 
-    data class Rock(val shape: List<Int>)
-
     class RockFactory {
-        val rocks = listOf(
+        private val rocks = listOf(
             Rock(listOf(15)),
             Rock(listOf(2, 7, 2)),
             Rock(listOf(7, 4, 4)),
             Rock(listOf(1, 1, 1, 1)),
             Rock(listOf(3, 3)))
-        var current = 0
+        private var current = 0
         fun next() = rocks[current++ % 5]
         fun nextIndex() = current % 5
     }
 
-    class Jet(val directions: String) {
-        var current = 0
+    class Jet(private val directions: String) {
+        private var current = 0
         fun next() = directions[current++ % directions.length]
         fun nextIndex() = current % directions.length
     }
